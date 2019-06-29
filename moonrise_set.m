20e0ef660d47476447128a002cd307c37f513c9a
Chris Gorman <chrisjohgorman@gmail.com>
2019-06-28 20:43:47 -0400

    using RA from moon.m requires conversion to hours, so divide by 15 when computing UT_Moon_in_south this function now gets close to pyephem's times.

--- moonrise_set.m
+++ moonrise_set.m
@@ -1,21 +1,27 @@
-function [moonise, moonset] = moonrise_set (year, month, day, latitude, longitude)
-	h = -0.833;
-	day_number = day_number(year, month, day);
-	[GMST0, RA, Decl] = moon (day_number, latitude, longitude, 0); 
-	UT_Sun_in_south = (RA - GMST0 - longitude) / 15.0;
-	UT_Sun_in_south = revolve_hour_angle(UT_Sun_in_south);
-	cos_lha = (sind(h) - sind(latitude)*sind(Decl))/(cosd(latitude) * cosd(Decl));
+function [moonrise, moonset] = moonrise_set (year, month, day, hour, latitude, longitude)
+
+	d = day_number(year, month, day, hour);
+	[x1, y1, z1, oblecl, L] = sun_rectangular(d)
+	[RA, Decl, r, az, alt] = moon(d, latitude, longitude)
+	mpar = asind(1/r)
+	GMST0 = (L + 180)
+	UT_Moon_in_south = RA/15 - (L+180)/15 - longitude/15.0
+	cos_lha = (sind(-mpar) - sind(latitude)*sind(Decl))/(cosd(latitude) * cosd(Decl))
 	if (cos_lha > 1)
-		error("Sun is always below our altitude limit.");
+		error("Moon is always below our altitude limit.");
 	elseif (cos_lha < -1)
-		error("Sun is always above our altitude limit.");
+		error("Moon is always above our altitude limit.");
 	end
-	LHA = acosd(cos_lha)/15;
+	LHA = acosd(cos_lha)/15.04107
 	
 	time = localtime(time);
 	
-	mr = UT_Sun_in_south - LHA + time.gmtoff/3600;
-	ms = UT_Sun_in_south + LHA + time.gmtoff/3600;
-	moonrise = datestr(sr/24, 'HH:MM');
-	moonset = datestr(ss/24, 'HH:MM');
+	mr = UT_Moon_in_south - LHA
+	ms = UT_Moon_in_south + LHA
+	%mr = UT_Moon_in_south - LHA + time.gmtoff/3600;
+	%ms = UT_Moon_in_south + LHA + time.gmtoff/3600;
+	moonrise = mr;
+	moonset = ms;
+	%moonrise = datestr(mr/24, 'HH:MM:SS');
+	%moonset = datestr(ms/24, 'HH:MM:SS');
 end
