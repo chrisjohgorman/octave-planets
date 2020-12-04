c2c9a86f6b48294bcd85dc8f4f39ad1fc659435f
Chris Gorman <chrisjohgorman@gmail.com>
2020-12-04 16:04:57 -0500

    Fix indentation error

--- eccentric_anomaly.m
+++ eccentric_anomaly.m
@@ -1,7 +1,7 @@
 function [e1] = eccentric_anomaly(M, e, tol)
     e0 = M + (180/pi) * e * sind(M) * (1 + e + cosd(M));
-        e1 = e0 - (e0 - (180/pi) * e * sind(e0) - M) / (1 - e * cosd(e0));
-        while abs(e0-e1) > tol
+    e1 = e0 - (e0 - (180/pi) * e * sind(e0) - M) / (1 - e * cosd(e0));
+    while abs(e0-e1) > tol
         e0 = e1;
         e1 = e0 - (e0 - (180/pi) * e * sind(e0) -M) / (1 -e * cosd(e0));
     end
