d1c86c4f973d3986d91696bb92b6a12bd62fc89a
Chris Gorman <chrisjohgorman@gmail.com>
2021-04-12 14:26:07 -0400

    Remove unnecessary hour variable

--- saturnrise_set.m
+++ saturnrise_set.m
@@ -1,6 +1,6 @@
-function [saturnrise, saturnset] = saturnrise_set (year, month, day, hour, latitude, longitude)
+function [saturnrise, saturnset] = saturnrise_set (year, month, day, latitude, longitude)
     h = -0.833;
-    d = day_number(year, month, day, hour);
+    d = day_number(year, month, day);
     [x1, y1, z1, oblecl, L] = sun_rectangular(d);
     [RA, Decl, r, az, alt] = saturn(d, latitude, longitude);
     GMST0 = (L + 180) / 15;
